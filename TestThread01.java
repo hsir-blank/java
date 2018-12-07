ab08cb9c2554eb39abbe0efd0b23f4080a6308c8
hansir <827278194@qq.com>
2018-12-07 13:36:58 +0800

    yield and sleep

--- TestThread01.java
+++ TestThread01.java
@@ -5,24 +5,38 @@ public class TestThread01 {
         t2.setName("T2***");
         t2.start();
 
-        CreatThread ct = new CreatThread();
+        Test01 ct = new Test01();  // sleep线程
         new Thread(ct, "t1").start();
 
         for (int i = 0; i < 200; i++) {
             System.out.println(Thread.currentThread().getName() + "---->" + i);
             if (i % 20 == 0) {
-                Thread.yield();
+                Thread.yield();  //yield让出时间片
             }
         }
     }
 }
-
 
 class CreatThread implements Runnable {
 
     @Override
     public void run() {
         for (int i = 0; i < 500; i++) {
+            System.out.println(Thread.currentThread().getName() + "---->" + i);
+
+        }
+    }
+}
+
+class Test01 implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 50; i++) {
+            try {
+                Thread.sleep(100);  // sleep
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println(Thread.currentThread().getName() + "---->" + i);
 
         }
